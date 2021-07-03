e7e6d96930550f25da9eb941eedb8a6f33c6e2d7
Serhii <serhii228228@gmail.com>
2021-07-03 16:42:39 +0200

    задали пользователю 2 вопроса на которые он дал ответ

--- first.js
+++ first.js
@@ -7,4 +7,12 @@ const personalMovieDB ={
     genres:[],
     privat:false
 };
-console.log(personalMovieDB.count);
+console.log(personalMovieDB);
+
+const a = prompt("Один тз последнтх просмотреных фильмов?");
+const a1 = prompt("Один тз последнтх просмотреных фильмов?");
+const l = prompt("На сколько оцените его");
+const l1 = prompt("На сколько оцените его");
+personalMovieDB.movies[a] = l;
+personalMovieDB.movies[a1] = l1;
+console.log(personalMovieDB.movies);
